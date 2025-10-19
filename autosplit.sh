9cc78bf07a91d06a65fd6ca954f7b6d4862b1ac4
loop <126372784+tribes2@users.noreply.github.com>
2025-10-19 03:47:23 +0100

    Update autosplit to do everything, including zipping and short circuiting if the repo is small enough

--- autosplit.sh
+++ autosplit.sh
@@ -1,43 +1,72 @@
 #!/bin/bash
 
-# 1.95GiB max size to avoid hitting GitHub ceilings
+# 1.95GiB max size to avoid hitting GitHub and Tribes 2 ceilings
+# Might be too high if GitHub uses GB instead of GiB, let me know
 SIZE_LIMIT=2093790080
+
+EXCLUSIONS="$1"
+ARTIFACT_PREFIX="$2"
+
+PATH_SIZE=$(du -b . | tail -n1 | cut -f1)
+
+direct_zip() {
+    # Convert comma-separated string to 'zip -x "pattern"' arguments
+    EXCLUDE_ARGS=""
+    IFS=',' read -ra PATTERNS <<< "$EXCLUSIONS"
+    for P in "${PATTERNS[@]}"; do
+        P_TRIMMED=$(echo "$P" | xargs)
+        if [ -n "$P_TRIMMED" ]; then
+        EXCLUDE_ARGS="$EXCLUDE_ARGS -x \"$P_TRIMMED\""
+        fi
+    done
+    
+    # Execute the zip command using 'eval' to properly handle multiple exclusion arguments
+    eval "zip -0 -r ${ARTIFACT_PREFIX}.vl2 . $EXCLUDE_ARGS"
+}
+
+if [[ $PATH_SIZE -lt $SIZE_LIMIT ]]; then
+    # short circuit to faster mechanism that doesn't involve stat (slow when the repo has thousands of small files)
+    direct_zip
+    exit 0
+fi
+
 CURRENT_SIZE=0
 LIST_FILE_COUNT=1
 
-# Check for the required folder argument
-if [ -z "$1" ]; then
-    echo "Usage: $0 <folder_path>"
-    exit 1
-fi
-
-FOLDER_PATH="$1"
+FIND_EXCLUDES=()
+IFS=',' read -r -a PATTERNS_ARRAY <<< "$EXCLUSIONS"
+for pattern in "${PATTERNS_ARRAY[@]}"; do
+    FIND_EXCLUDES+=( -not -path "$pattern" )
+done
 
 next_list() {
+    if [[ $LIST_FILE_COUNT -ne 1 ]]; then
+        LAST_COUNT=$(($LIST_FILE_COUNT-1))
+        echo "Zipping archive part ${LAST_COUNT}"
+        zip -0 -@ ${ARTIFACT_PREFIX}-part${LAST_COUNT}.vl2 < $LIST_FILENAME > /dev/null
+        rm $LIST_FILENAME
+    fi
     LIST_FILENAME="file_list_${LIST_FILE_COUNT}.txt"
+    echo "Collecting files for vl2 part $LIST_FILENAME"
     LIST_FILE_COUNT=$((LIST_FILE_COUNT + 1))
     CURRENT_SIZE=0
 }
 
 next_list
 
-find "$FOLDER_PATH" -type f -print0 | while IFS= read -r -d $'\0' file; do
+find . -type f -print0 | while IFS= read -r -d $'\0' file; do
     FILE_SIZE=$(stat -c%s "$file" 2>/dev/null)
-
     if [ -z "$FILE_SIZE" ]; then
-        echo "Warning: Could not get size for file: $file. Skipping." >> "file_list_errors.log"
+        echo "Warning: Could not get size for file: $file. Skipping."
         continue
     fi
-
     if (( CURRENT_SIZE + FILE_SIZE > SIZE_LIMIT )); then
         next_list
     fi
-    
-    echo "$FILE_SIZE $file" >> "$LIST_FILENAME"
-    
+    echo "$file" >> "$LIST_FILENAME"
     # Update the current size
     CURRENT_SIZE=$((CURRENT_SIZE + FILE_SIZE))
 
 done
 
-exit $LIST_FILE_COUNT
+next_list
